d250a4b64e1422e1efe8def31ebd62a12081c517
dwzyx <54096206+dwzyx@users.noreply.github.com>
2019-08-14 22:56:54 +0800

    Add files via upload

---
+++ User.java
@@ -0,0 +1,92 @@
+package com.sclg.lrp.domain;
+
+public class User {
+
+    private int userID;
+    private String userName;
+    private String userSex;
+    private int userGrade;
+    private String userDept;
+    private String userTech;
+    private String userSingle;
+    private int userNum;
+    private String userPassword;
+
+    //以上为用户序号、用户名、用户性别、年级、部门、技术、是否单身、学号（账号）、密码
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(String userSex) {
+        this.userSex = userSex;
+    }
+
+    public int getUserGrade() {
+        return userGrade;
+    }
+
+    public void setUserGrade(int userGrade) {
+        this.userGrade = userGrade;
+    }
+
+    public String getUserDept() {
+        return userDept;
+    }
+
+    public void setUserDept(String userDept) {
+        this.userDept = userDept;
+    }
+
+    public String getUserTech() {
+        return userTech;
+    }
+
+    public void setUserTech(String userTech) {
+        this.userTech = userTech;
+    }
+
+    public String getUserSingle() {
+        return userSingle;
+    }
+
+    public void setUserSingle(String usersingle) {
+        this.userSingle = userSingle;
+    }
+
+    public int getUserNum() {
+        return userNum;
+    }
+
+    public void setUserNum(int userNum) {
+        this.userNum = userNum;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+
+
+
+}
