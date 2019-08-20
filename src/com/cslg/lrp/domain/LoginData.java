97e81ea4dd6e505efed21fd524709547ea6cac1b
MLP <1033430415@qq.com>
2019-08-20 16:59:00 +0800

    service

--- src/com/cslg/lrp/domain/LoginData.java
+++ src/com/cslg/lrp/domain/LoginData.java
@@ -1,30 +1,30 @@
 package com.cslg.lrp.domain;
 
 /**
- * 登录模型,封装了用户名和密码
+ * 登录模型,封装了学号和密码
  */
 public class LoginData {
 
-    // 用户名
-    private String userName;
+    // 学号
+    private String userId;
 
-    //密码
+    // 密码
     private String password;
 
     public LoginData() {
     }
 
     public LoginData(String userName, String password) {
-        this.userName = userName;
+        this.userId = userName;
         this.password = password;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getUserId() {
+        return userId;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
     public String getPassword() {
